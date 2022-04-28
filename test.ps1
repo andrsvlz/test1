b25badfbefc83bb0d610df14efd224c1e01f0a31
andrsvlz <andrsvlz@gmail.com>
2022-04-28 12:14:15 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -265,7 +265,7 @@ $exit_code = Run-Process -executable $folder"dotnet.exe" -arguments "/quiet /nor
 
 $architecture = $env:PROCESSOR_ARCHITECTURE
 if ($architecture -eq "AMD64") {
- if (Get-ItemProperty -Path "HKLM:\SOFTWARE\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate\Clients\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}") 
+ if (Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate\Clients\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}") 
  {
 instalado}
 else{
